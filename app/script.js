150614292dfa2119d09b2740a5dec912a60fbadd
Henrique Rodrigues Orefice <h.orefice@gmail.com>
2020-03-11 21:17:15 -0300

    tela com smartphone implementada

--- app/script.js
+++ app/script.js
@@ -1,5 +1,10 @@
-'use strict';
+$(document).ready(function() {
+  // Go to first content screen
+  $(".tela1").show()
 
-const hello = <h1>Hello, world!</h1>;
-
-ReactDOM.render(hello, document.getElementById('root'));
+  // Go to another screen buttons
+  $("#goto2").click(function() {
+    $(".tela1").hide(1000);
+    $(".tela2").show(1000);
+  });
+});
