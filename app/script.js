fa4824bd2a26d2f6ebd146fe1617b8508adad635
Henrique Rodrigues Orefice <h.orefice@gmail.com>
2020-03-18 20:23:00 -0300

    primeira tela organizada

--- app/script.js
+++ app/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   var DELAY = 700;
+  var nome = "Viagem para Paris"
+  var valor = 6500
+  var mes = 12
+  var ano = 2020
 
   // Go to first content screen
   $(".tela1").show()
@@ -8,19 +12,19 @@ $(document).ready(function() {
   $("#goto2").click(function() {
     $(".tela1").fadeOut(DELAY);
     $(".tela2").delay(DELAY).fadeIn(DELAY);
-    nome = $("#namme").val();
-    valor = parseFloat($("#valor").val());
-    mes = parseInt($("#Mes").val());
-    ano = parseInt($("#Ano").val());
 
-    $("#sonhoname").html(nome);
+    nome = $("#nome").val();
+    valor = parseFloat($("#valor_total").val());
+    mes = parseInt($("#mes").val());
+    ano = parseInt($("#ano").val());
+
+    // $("#sonhoname").html(nome);
     var d = new Date();
     curr_m = d.getMonth() + 1;
     curr_y = d.getFullYear();
-    tempo = (mes - curr_m) + 12*(ano - curr_y)
+    tempo = (mes - curr_m) + 12 * (ano - curr_y)
     per_month = parseFloat(valor/tempo).toFixed(2);
     // $("#sonhoname").html(nome);
-    $("#flay").html('R$ ' + per_month + ' por mês');
-
+    $("#valor_mensal").html('R$ ' + per_month + ' por mês');
   });
 });
