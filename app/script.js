19446842e3dc72d4c154e2a405e2d038f37047aa
LucasBamidele <bamidele.lucas@gmail.com>
2020-03-18 20:03:14 -0300

    calculo economia mensal

--- app/script.js
+++ app/script.js
@@ -8,5 +8,19 @@ $(document).ready(function() {
   $("#goto2").click(function() {
     $(".tela1").fadeOut(DELAY);
     $(".tela2").delay(DELAY).fadeIn(DELAY);
+    nome = $("#namme").val();
+    valor = parseFloat($("#valor").val());
+    mes = parseInt($("#Mes").val());
+    ano = parseInt($("#Ano").val());
+
+    $("#sonhoname").html(nome);
+    var d = new Date();
+    curr_m = d.getMonth() + 1;
+    curr_y = d.getFullYear();
+    tempo = (mes - curr_m) + 12*(ano - curr_y)
+    per_month = parseFloat(valor/tempo).toFixed(2);
+    // $("#sonhoname").html(nome);
+    $("#flay").html('R$ ' + per_month + ' por mês');
+
   });
 });
